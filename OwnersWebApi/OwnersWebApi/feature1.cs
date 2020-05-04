46cc79b415fb0ee7696c7db146806bf9a3233ab5
albesp <a.spinos@hotmail.com>
2020-05-04 18:22:13 -0500

    Changes Add Method

--- OwnersWebApi/OwnersWebApi/feature1.cs
+++ OwnersWebApi/OwnersWebApi/feature1.cs
@@ -8,12 +8,9 @@ namespace OwnersWebApi
     public class feature1
     {
 
-        public int Add()
-        {
-            int x1 = 1;
-            int x2 = 2;
-            int sum = x1 + x2;
-            return sum;
+        public int Add(int x1, int x2)
+        {            
+            return x1 + x2;
         }
     }
 }
